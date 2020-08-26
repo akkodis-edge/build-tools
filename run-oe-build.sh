aaab5a626e6508a0da77a574bdb09bd3377788f3
Mikko Salomäki <ms@datarespons.se>
2020-08-26 07:46:22 +0200

    add helper script for starting oe-build image

---
+++ run-oe-build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Run a oe-build image as current user with access to source folder and optionally with system wide SSTATE and DOWNLOADS
+#
+
+set -e
+
+# Parse arguments
+source_dir=""
+sstate_dir=""
+downloads_dir=""
+while getopts "h?s:c:d:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Usage: $(basename $0) [OPTIONS]"
+        echo
+        echo "Options:"
+        echo " -c      Source directory to pass into container"
+        echo " -s      sstate-cache directory to pass into container"
+        echo " -d      downloads directory to pass into container"
+        echo
+        exit 0
+        ;;
+    c)  source_dir="$(realpath $OPTARG)"
+        ;;
+    d)	downloads_dir="$(realpath $OPTARG)"
+        ;;
+    s)	sstate_dir="$(realpath $OPTARG)"
+        ;;
+    esac
+done
+
+cmd="docker run -it"
+if [ ! -z ${source_dir} ]; then
+	cmd="$cmd -v $source_dir:$source_dir"
+fi
+if [ ! -z ${sstate_dir} ]; then
+	cmd="$cmd -v $sstate_dir:$sstate_dir"
+fi
+if [ ! -z ${downloads_dir} ]; then
+	cmd="$cmd -v $downloads_dir:$downloads_dir"
+fi
+cmd="$cmd oe:$(id -un)" # add name:tag of image
+echo "Running command:"
+echo "$cmd"
+$cmd
